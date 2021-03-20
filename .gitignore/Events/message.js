5bc73e5edfa877ed945f1a584605ffa8747e038a
RedCode <56961931+EzeeyGHO@users.noreply.github.com>
2021-03-20 10:35:32 +0100

    Update message.js

--- .gitignore/Events/message.js
+++ .gitignore/Events/message.js
@@ -5,6 +5,7 @@ module.exports = async(client, message) => {
 
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
+    if(message.guild.channels.cache.get("818024721798397982"))
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const commande = args.shift();
